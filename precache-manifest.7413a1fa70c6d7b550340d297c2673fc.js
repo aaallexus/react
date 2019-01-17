d0f784848975995ffcad1930b6afea08b063383a
aaallexus <alexei@falko.com.ua>
2019-01-17 06:12:47 +0200

    Redux

---
+++ precache-manifest.7413a1fa70c6d7b550340d297c2673fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9ebc78b97eeeb8b3b0e7",
+    "url": "/static/js/main.9ebc78b9.chunk.js"
+  },
+  {
+    "revision": "63a90288e5fb34fb099e",
+    "url": "/static/js/1.63a90288.chunk.js"
+  },
+  {
+    "revision": "9ebc78b97eeeb8b3b0e7",
+    "url": "/static/css/main.f17f7ef5.chunk.css"
+  },
+  {
+    "revision": "00a8d677f9153f138d268a1f351495ee",
+    "url": "/index.html"
+  }
+];
